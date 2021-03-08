90a688329e4be9ca12d465201f84e54829e71137
Deepika Dabi <dabideepika1204@gmail.com>
2021-03-08 18:43:25 +0530

    Styled-Components-Added

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
-export default{
-    title: 'form/Button',
-    component: Button
-}
-export const Primay = () => <Button variant='primary'>Primary</Button>
-export const Secondary = () => <Button variant='secondary'>Secondary</Button>
-export const Success = () => <Button variant='success'>Success</Button>
-export const Danger = () => <Button variant='danger'>Danger</Button>
-export const Disabled = () => <Button variant='disabled'>Disabled</Button>
+export default {
+  title: "form/Button",
+  component: Button,
+};
+export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+export const Success = () => <Button variant="success">Success</Button>;
+export const Danger = () => <Button variant="danger">Danger</Button>;
+export const Disabled = () => <Button variant="disabled">Disabled</Button>;
 
-const Template =args => <Button {...args} />
+const Template = (args) => <Button {...args} />;
 
-export const PrimaryA = Template.bind({})
+export const PrimaryA = Template.bind({});
 PrimaryA.args = {
-    variant: 'primary',
-    children:'Primary Args'
-}
+  variant: "primary",
+  children: "Primary Args",
+};
