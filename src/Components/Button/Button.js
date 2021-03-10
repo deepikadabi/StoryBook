2d90118547f03b1d6db75ca7bd1c3405b493fc6f
Nihir Shah <nihir10dec@gmail.com>
2021-03-10 12:38:45 +0530

    styled components updated

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,47 +1,8 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
-//import './Button.css'
+import styled from 'styled-components'
 
-
-function Button (props) {
-    const {variant = 'primary', children} = props
-    let primary = false;
-    let secondary = false;
-    let success = false;
-    let danger = false;
-    let disabled = false;  
-    
-    switch (variant) {
-        case 'primary':
-            primary = true;
-            break;
-        case 'secondary':
-            secondary = true;
-            break;
-        case 'success':
-            success = true;
-            break;
-        case 'danger':
-            danger = true;
-            break;
-        case 'disabled':
-            disabled = true;
-            break;
-        default:
-            break;
-    }
-
-    return (   
-        <Button1 primary={primary} secondary={secondary} disabled={disabled} success={success} danger={danger}>  
-            {children}
-        </Button1>
-    )
-}
-
-export default Button
-
-const Button1 = styled.button`
-border: none;
+const CommonButton = styled.button `
+  border: none;
   color: white;
   padding: 5px 18px;
   text-align: center;
@@ -53,37 +14,32 @@ border: none;
   margin: 4px 2px;
   transition-duration: 0.4s;
   cursor: pointer;
+  `
 
-  ${props =>
-    props.primary &&
-    css`
-       background-color: white;
-        color: black;
-        border: 2px solid #555555;
+const PrimaryButton = styled(CommonButton)
+`
+background-color: white;
+color: black;
+border: 2px solid #555555;
+&:hover{
+    background-color: #555555;
+    color: white;
+}
+`
+
+const SecondaryButton = styled(CommonButton)
+`
+    background-color: white; 
+    color:#008CBA; 
+    border: 2px solid #008CBA;
     &:hover{
-        background-color: #555555;
+        background-color: #008CBA;
         color: white;
-    }
-
-    `};
-
-    ${props =>
-    props.secondary &&
-    css`
-           background-color: white; 
-            color:#008CBA; 
-            border: 2px solid #008CBA;
-        &:hover{
-            background-color: #008CBA;
-            color: white;
-        }
-
-    `};
+    }`
 
 
-    ${props =>
-    props.danger &&
-    css`
+const DangerButton = styled(CommonButton)
+`
        background-color: white; 
     color: #f44336; 
     border: 2px solid #f44336;
@@ -91,30 +47,43 @@ border: none;
     &:hover{
         background-color: #f44336;
         color: white;
-    }
-    `};
+    }`
 
 
-    ${props =>
-    props.disabled &&
-    css`
+const DisabledButton = styled(CommonButton)
+`
         background-color: white;
     color: gray;
     border: 2px solid #e7e7e7;
     &:hover{
         background-color: #e7e7e7;
-    }
-    `};
+    }`
 
-    ${props =>
-    props.success &&
-    css`
+const SuccessButton = styled(CommonButton)
+`
        background-color: white; 
         color: #4CAF50; 
         border: 2px solid #4CAF50;
     &:hover{
         background-color: #4CAF50;
         color: white;
+    }`
+
+export default function Button(props) {
+    const { variant = 'primary', children  } = props
+    const theme = {
+        'primary' : PrimaryButton,
+        'secondary' : SecondaryButton,
+        'success' : SuccessButton,
+        'danger' : DangerButton,
+        'disabled' : DisabledButton
     }
-    `};
-` 
+    const ThemeButton = theme[variant];
+    return ( 
+    <ThemeButton > 
+        {children}
+     </ThemeButton>
+    )
+}
+
+//Button
--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -2,19 +2,38 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-  title: "form/Button",
+  title: "form/CustomButton",
   component: Button,
 };
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
-export const Disabled = () => <Button variant="disabled">Disabled</Button>;
 
 const Template = (args) => <Button {...args} />;
 
-export const PrimaryA = Template.bind({});
-PrimaryA.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   variant: "primary",
   children: "Primary Args",
+};
+
+export const Seconday = Template.bind({});
+Seconday.args = {
+  variant: "secondary",
+  children: "secondary Args",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  variant: "success",
+  children: "success Args",
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  variant: "danger",
+  children: "danger Args",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  variant: "disabled",
+  children: "disabled Args",
 };
